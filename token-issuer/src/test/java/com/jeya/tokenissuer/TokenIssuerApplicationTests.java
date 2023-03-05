e2ea8f8e1805ee6327f88d1fae75775c1c116e77
JJeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2023-03-06 01:06:42 +0530

    first commit

---
+++ token-issuer/src/test/java/com/jeya/tokenissuer/TokenIssuerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeya.tokenissuer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenIssuerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
